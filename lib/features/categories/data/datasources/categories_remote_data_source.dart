20a17a4bb886883016bb0349afa6ce8f9598c2aa
thanh2242003 <thanhkma2242003@gmail.com>
2026-03-11 22:46:17 +0700

    feat: categories remote data source

---
+++ lib/features/categories/data/datasources/categories_remote_data_source.dart
@@ -0,0 +1,40 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:ecommerce_app/features/categories/data/models/category_model.dart';
+
+// abstract class CategoriesRemoteDataSource{
+//   Future<List<CategoryModel>> getCategories();
+// }
+//
+// class CategoriesRemoteDataSourceImpl implements CategoriesRemoteDataSource{
+//   final String baseUrl = "http:";
+//   @override
+//   Future<List<CategoryModel>> getCategories() async{
+//     final response = await http.get(Uri.parse('$baseUrl/api/categories'));
+//     if(response.statusCode == 200){
+//       final List<dynamic> data = json.decode(response.body);
+//       return data.map((e) => CategoryModel.fromJson(e)).toList();
+//     }else{
+//       throw Exception('Failed to load categories');
+//     }
+//   }
+// }
+
+// chua ket noi backend
+abstract class CategoriesRemoteDataSource {
+  Future<List<CategoryModel>> getCategories();
+}
+
+class CategoriesRemoteDataSourceImpl implements CategoriesRemoteDataSource {
+  @override
+  Future<List<CategoryModel>> getCategories() async {
+
+    await Future.delayed(const Duration(seconds: 1));
+
+    return [
+      CategoryModel(categoryId: "1", title: "Clothes", image: "clothes.png"),
+      CategoryModel(categoryId: "2", title: "Toys", image: "toys.png"),
+      CategoryModel(categoryId: "3", title: "Shoes", image: "shoes.png"),
+    ];
+  }
+}
