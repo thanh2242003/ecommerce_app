660c19c95a4021aeb3fb783c843e0fa7c2a773fa
thanh2242003 <thanhkma2242003@gmail.com>
2026-03-13 23:42:05 +0700

    feat(product): add product cubit logic

--- lib/features/product/presentation/bloc/product_cubit.dart
+++ lib/features/product/presentation/bloc/product_cubit.dart
@@ -1,6 +1,30 @@
-import 'package:ecommerce_app/features/product/presentation/bloc/product_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class ProductCubit extends Cubit<ProductState>{
-  ProductCubit(): super (ProductInitial());
+import '../../domain/usecases/get_top_selling_products.dart';
+import 'product_state.dart';
+
+class ProductCubit extends Cubit<ProductState> {
+
+  final GetTopSellingProducts getTopSellingProducts;
+
+  ProductCubit(this.getTopSellingProducts) : super(ProductInitial());
+
+  void loadProducts() async {
+
+    try {
+
+      emit(ProductLoading());
+
+      final products = await getTopSellingProducts();
+
+      emit(ProductLoaded(products));
+
+    } catch (e) {
+
+      emit(ProductError(e.toString()));
+
+    }
+
+  }
+
 }
