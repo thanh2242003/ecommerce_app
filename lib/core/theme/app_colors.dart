364a482817d340037ddc0543181ecfe4093af866
thanh2242003 <thanhkma2242003@gmail.com>
2026-03-15 00:01:56 +0700

    style(theme):add secondColor in app color

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const primaryColor = Color(0xFFff5722);
+  static const secondColor = Color(0x12342f3f);
 
   static const lightBackground = Colors.white;
   static const darkBackground = Color(0xFF121212);
