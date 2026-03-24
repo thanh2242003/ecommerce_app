96e27f1560e111bc9d58abd46ec2b681c5888b0b
thanh2242003 <thanhkma2242003@gmail.com>
2026-03-24 22:04:39 +0700

    feat(product): add getProductsByTitle to product repository

--- lib/features/product/domain/repositories/product_repository.dart
+++ lib/features/product/domain/repositories/product_repository.dart
@@ -4,5 +4,6 @@ abstract class ProductRepository{
   Future<List<ProductEntity>> getTopSellingProducts();
 
   Future<List<ProductEntity>> getProductsDetail(String productId);
-
+  Future<List<ProductEntity>> getProductsByTitle(String title);
+  //Future<List<ProductEntity>> getNewProducts({int limit = 10});
 }
