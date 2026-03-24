eac8d702599b1a5e076b489ba5db9fca4d92cb40
thanh2242003 <thanhkma2242003@gmail.com>
2026-03-24 21:53:37 +0700

    feat(auth): add user data and user state to auth state

--- lib/features/auth/presentation/bloc/auth_state.dart
+++ lib/features/auth/presentation/bloc/auth_state.dart
@@ -1,5 +1,7 @@
 import 'package:equatable/equatable.dart';
 
+import '../../data/models/user_model.dart';
+
 abstract class AuthState extends Equatable {
   @override
   List<Object?> get props => [];
@@ -9,6 +11,24 @@ class AuthInitial extends AuthState {}
 
 class AuthLoading extends AuthState {}
 
-class AuthAuthenticated extends AuthState {}
+class AuthAuthenticated extends AuthState {
+  final UserModel user;
+  final String accessToken;
+  final String refreshToken;
+
+  AuthAuthenticated(this.user, this.accessToken, this.refreshToken);
+
+  @override
+  List<Object?> get props => [user, accessToken, refreshToken];
+}
 
 class AuthUnauthenticated extends AuthState {}
+
+class AuthError extends AuthState {
+  final String message;
+
+  AuthError(this.message);
+
+  @override
+  List<Object?> get props => [message];
+}
